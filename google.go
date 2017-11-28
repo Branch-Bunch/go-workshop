85e765a77ecdefe3a2c7c48920246ad6de69004d
Jacky Chiu <jackyzhanchiu@gmail.com>
2017-11-28 15:47:08 -0500

    Google exmaple (#6)
    * Google search examples
    
    * CONCURRENT

---
+++ google.go
@@ -0,0 +1,51 @@
+// +build OMIT
+
+// The comments in these files for the presentation tool used to generate the slides.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Result string
+
+func Google(query string) []Result {
+	var results []Result
+
+	results = append(results, Web(query))
+	results = append(results, Image(query))
+	results = append(results, Video(query))
+
+	return results
+}
+
+// START1 OMIT
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+type Search func(query string) Result // HL
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+// STOP1 OMIT
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang") // HL
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+
+}
---
+++ google_concurrent.go
@@ -0,0 +1,52 @@
+// +build OMIT
+// The comments in these files for the presentation tool used to generate the slides.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Result string
+
+func Google(query string) []Result {
+	var results []Result
+	resultsChan := make(chan Result)
+
+	go func() { resultsChan <- Web(query) }()
+	go func() { resultsChan <- Image(query) }()
+	go func() { resultsChan <- Video(query) }()
+
+	for i := 0; i < 3; i++ {
+		results = append(results, <-resultsChan)
+	}
+
+	return results
+}
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+
+}
